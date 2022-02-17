442ff772e20226f3a1bfc6be1bdca772f4cddb42
Eduardo <edreygar@gmail.com>
2022-02-17 14:54:46 -0600

    act function planteles equivalencia clave asignar

--- funciones/2021/planteles_equivalencia_clave_asignar.sql
+++ funciones/2021/planteles_equivalencia_clave_asignar.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION planteles_equivalencia_clave_asignar(_id_empleado int, _id_estructura_ocupacional int, _hsm int, _id_cat_categoria int, _categoria_padre character varying, _alta int)
+CREATE OR REPLACE FUNCTION planteles_equivalencia_clave_asignar(_id_empleado int, _id_estructura_ocupacional int, _hsm int, _id_cat_categoria int, _categoria_padre character varying, _alta int, _id_componente int, _id_materia int, _id_subprograma int)
 RETURNS TABLE(
     id_cat_categoria_padre integer,
     categoria_padre character varying,
@@ -11,6 +11,10 @@ RETURNS TABLE(
 AS $$
 
 DECLARE
+
+    _id_categoria_equivalente int;
+    _categoria_equivalente text;
+    _es_orientacion int;
 
     reg RECORD;
 	cur CURSOR FOR (
@@ -85,7 +89,7 @@ DECLARE
             INNER JOIN cat_categorias_padre b ON x.categoria_padre = b.categoria_padre 
             WHERE id_empleado = _id_empleado
             AND id_estructura_ocupacional = _id_estructura_ocupacional
-            AND licencia = 0
+            --AND licencia = 0
             AND baja = 0
             AND x.id_plantilla_base_docente_rh = a.id_plantilla_base_docente_rh
             GROUP BY
@@ -111,6 +115,7 @@ DECLARE
     INNER JOIN cat_categorias_padre b ON a.categoria_padre = b.categoria_padre 
     WHERE a.id_estructura_ocupacional = _id_estructura_ocupacional
     AND a.id_empleado = _id_empleado
+    AND a.id_subprograma = _id_subprograma
     GROUP BY
         a.id_plantilla_base_docente_rh,
         b.id_cat_categoria_padre, 
@@ -122,6 +127,29 @@ DECLARE
 
 
 BEGIN
+
+    _es_orientacion:= 0;
+    _id_categoria_equivalente:= 78;
+    _categoria_equivalente:= 'TCBI';
+
+    -- Saber si se cambia la categoria dependiendo de la descarga y de la materia
+    SELECT INTO _es_orientacion
+        id_detalle_materia
+    FROM detalle_materias a
+    INNER JOIN cat_materias b ON a.id_materia = b.id_materia
+    WHERE
+        a.id_detalle_materia = _id_materia
+        AND palabra_acentos(b.materia) ilike palabra_acentos('%ORIENTACION%');
+
+    IF _id_componente in (1,2,3) THEN
+        
+        IF _es_orientacion = 0 OR _es_orientacion is null THEN
+            _id_categoria_equivalente:= 13;
+            _categoria_equivalente:= 'CBI';
+        END IF;
+
+    END IF;    
+
 
     CREATE TEMP TABLE temp_equivalencia_clave_asignar(
         id_cat_categoria_padre integer,
@@ -135,19 +163,28 @@ BEGIN
 
     FOR reg IN cur LOOP
 
+        -- Saber si hago cambio de clave o no
+        IF (reg.id_cat_categoria_padre NOT IN (78, 79)) THEN
+
+            _id_categoria_equivalente:= reg.id_cat_categoria_padre;
+            _categoria_equivalente:= reg.categoria_padre;
+
+        END IF;
+
+
         IF(_hsm > 0) THEN
 
             IF(_hsm >= (reg.hrs_categoria - (reg.hrs_usadas + reg.hrs_usadas_tf))) THEN
 
                 INSERT INTO temp_equivalencia_clave_asignar(id_cat_categoria_padre, categoria_padre, hrs_categoria, hrs_usadas, hrs_libres, alta, id_plantilla_base_docente_rh) VALUES
-                (reg.id_cat_categoria_padre, reg.categoria_padre, reg.hrs_categoria, reg.hrs_usadas, (reg.hrs_categoria - (reg.hrs_usadas + reg.hrs_usadas_tf)), 2, reg.id_plantilla_base_docente_rh);
+                (_id_categoria_equivalente, _categoria_equivalente, reg.hrs_categoria, reg.hrs_usadas, (reg.hrs_categoria - (reg.hrs_usadas + reg.hrs_usadas_tf)), 2, reg.id_plantilla_base_docente_rh);
 
                 _hsm:= _hsm - (reg.hrs_categoria - (reg.hrs_usadas + reg.hrs_usadas_tf));
 
             ELSIF( (reg.hrs_categoria - (reg.hrs_usadas + reg.hrs_usadas_tf)) > _hsm) THEN
 
                 INSERT INTO temp_equivalencia_clave_asignar(id_cat_categoria_padre, categoria_padre, hrs_categoria, hrs_usadas, hrs_libres, alta, id_plantilla_base_docente_rh) VALUES
-                (reg.id_cat_categoria_padre, reg.categoria_padre, reg.hrs_categoria, reg.hrs_usadas, _hsm, 2, reg.id_plantilla_base_docente_rh);
+                (_id_categoria_equivalente, _categoria_equivalente, reg.hrs_categoria, reg.hrs_usadas, _hsm, 2, reg.id_plantilla_base_docente_rh);
 
                 _hsm:= _hsm - (reg.hrs_categoria - (reg.hrs_usadas + reg.hrs_usadas_tf));
 
@@ -157,6 +194,7 @@ BEGIN
 
     END LOOP;
 
+    -- Si no tiene horas de descarga entra aqui
     IF( _hsm > 0) THEN
 
          INSERT INTO temp_equivalencia_clave_asignar(id_cat_categoria_padre, categoria_padre, hrs_categoria, hrs_usadas, hrs_libres, alta, id_plantilla_base_docente_rh) VALUES
---
+++ funciones/2022/view_nombramientos_planeacion_tf.sql
@@ -0,0 +1,99 @@
+CREATE VIEW view_nombramientos_planeacion_tf AS
+(
+SELECT 
+	nomb.id_nombramiento_planeacion_tf,
+	estruc.id_estructura_ocupacional,
+	estruc.periodo,
+	nomb.no_oficio,
+	nomb.id_empleado_nombramiento,
+	emp1.nombre||' '||emp1.paterno||' '||emp1.materno AS empleado_nombramiento,
+	detalle.id_detalle_materia,
+	0 AS id_paraescolar,
+	cat_materia.materia,
+	materias.hrs_materia,
+	materias.fecha_desde,
+	materias.fecha_hasta,
+	nomb.id_empleado_director_general,
+	emp2.nombre||' '||emp2.paterno||' '||emp2.materno AS empleado_director_general,
+	nomb.id_empleado_director_planeacion,
+	emp3.nombre||' '||emp3.paterno||' '||emp3.materno AS empleado_director_planeacion,
+	nomb.id_empleado_delegado_admvo,
+	emp4.nombre||' '||emp4.paterno||' '||emp4.materno AS empleado_delegado_admvo,
+	nomb.id_empleado_coordinador_sectorial,
+	emp5.nombre||' '||emp5.paterno||' '||emp5.materno AS empleado_coordinador_sectorial,
+	nomb.id_empleado_director,
+	emp6.nombre||' '||emp6.paterno||' '||emp6.materno AS empleado_director,
+	nomb.id_empleado_jefe_rh,
+	emp7.nombre||' '||emp7.paterno||' '||emp7.materno AS empleado_jefe_rh,
+	usr.id_usuario,
+	emp8.nombre||' '||emp8.paterno||' '||emp8.materno AS usuario_captura,
+	nomb.fecha_creacion
+FROM nombramientos_planeacion_tf nomb
+INNER JOIN nombramientos_planeacion_tf_materias materias ON materias.id_nombramiento_planeacion_tf = nomb.id_nombramiento_planeacion_tf
+INNER JOIN detalle_materias detalle ON detalle.id_detalle_materia = materias.id_detalle_materia
+INNER JOIN cat_materias cat_materia ON cat_materia.id_materia = detalle.id_materia
+INNER JOIN empleados emp1 ON emp1.id_empleado = nomb.id_empleado_nombramiento
+INNER JOIN subprogramas subp ON subp.id_subprograma = nomb.id_subprograma
+INNER JOIN cat_estructuras_ocupacionales estruc ON estruc.id_estructura_ocupacional = nomb.id_estructura_ocupacional
+INNER JOIN usuarios usr ON usr.id_usuario = nomb.id_usuario_captura
+INNER JOIN empleados emp8 ON emp8.id_empleado = usr.id_empleado
+LEFT JOIN empleados emp2 ON emp2.id_empleado = nomb.id_empleado_director_general
+LEFT JOIN empleados emp3 ON emp3.id_empleado = nomb.id_empleado_director_planeacion
+LEFT JOIN empleados emp4 ON emp4.id_empleado = nomb.id_empleado_delegado_admvo
+LEFT JOIN empleados emp5 ON emp5.id_empleado = nomb.id_empleado_coordinador_sectorial
+LEFT JOIN empleados emp6 ON emp6.id_empleado = nomb.id_empleado_director
+LEFT JOIN empleados emp7 ON emp7.id_empleado = nomb.id_empleado_jefe_rh
+ORDER BY
+	nomb.id_estructura_ocupacional,
+	nomb.id_empleado_nombramiento,
+	cat_materia.materia
+)
+UNION ALL
+(
+SELECT 
+	nomb.id_nombramiento_planeacion_tf,
+	estruc.id_estructura_ocupacional,
+	estruc.periodo,
+	nomb.no_oficio,
+	nomb.id_empleado_nombramiento,
+	emp1.nombre||' '||emp1.paterno||' '||emp1.materno AS empleado_nombramiento,
+	0 AS id_detalle_materia,
+	cat_materia.id_paraescolar,
+	cat_materia.nombre AS materia,
+	materias.hrs_materia,
+	materias.fecha_desde,
+	materias.fecha_hasta,
+	nomb.id_empleado_director_general,
+	emp2.nombre||' '||emp2.paterno||' '||emp2.materno AS empleado_director_general,
+	nomb.id_empleado_director_planeacion,
+	emp3.nombre||' '||emp3.paterno||' '||emp3.materno AS empleado_director_planeacion,
+	nomb.id_empleado_delegado_admvo,
+	emp4.nombre||' '||emp4.paterno||' '||emp4.materno AS empleado_delegado_admvo,
+	nomb.id_empleado_coordinador_sectorial,
+	emp5.nombre||' '||emp5.paterno||' '||emp5.materno AS empleado_coordinador_sectorial,
+	nomb.id_empleado_director,
+	emp6.nombre||' '||emp6.paterno||' '||emp6.materno AS empleado_director,
+	nomb.id_empleado_jefe_rh,
+	emp7.nombre||' '||emp7.paterno||' '||emp7.materno AS empleado_jefe_rh,
+	usr.id_usuario,
+	emp8.nombre||' '||emp8.paterno||' '||emp8.materno AS usuario_captura,
+	nomb.fecha_creacion
+FROM nombramientos_planeacion_tf nomb
+INNER JOIN nombramientos_planeacion_tf_materias_paraescolares materias ON materias.id_nombramiento_planeacion_tf = nomb.id_nombramiento_planeacion_tf
+INNER JOIN cat_materias_paraescolares cat_materia ON cat_materia.id_paraescolar = materias.id_paraescolar
+INNER JOIN empleados emp1 ON emp1.id_empleado = nomb.id_empleado_nombramiento
+INNER JOIN subprogramas subp ON subp.id_subprograma = nomb.id_subprograma
+INNER JOIN cat_estructuras_ocupacionales estruc ON estruc.id_estructura_ocupacional = nomb.id_estructura_ocupacional
+INNER JOIN usuarios usr ON usr.id_usuario = nomb.id_usuario_captura
+INNER JOIN empleados emp8 ON emp8.id_empleado = usr.id_empleado
+LEFT JOIN empleados emp2 ON emp2.id_empleado = nomb.id_empleado_director_general
+LEFT JOIN empleados emp3 ON emp3.id_empleado = nomb.id_empleado_director_planeacion
+LEFT JOIN empleados emp4 ON emp4.id_empleado = nomb.id_empleado_delegado_admvo
+LEFT JOIN empleados emp5 ON emp5.id_empleado = nomb.id_empleado_coordinador_sectorial
+LEFT JOIN empleados emp6 ON emp6.id_empleado = nomb.id_empleado_director
+LEFT JOIN empleados emp7 ON emp7.id_empleado = nomb.id_empleado_jefe_rh
+ORDER BY
+	nomb.id_estructura_ocupacional,
+	nomb.id_empleado_nombramiento,
+	cat_materia.nombre
+)
